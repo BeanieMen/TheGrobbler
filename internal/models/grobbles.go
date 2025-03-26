57f20c5b00ec0ceb11e1869c777dc7cefc26f6e3
BeanieMen <jainarjav886@gmail.com>
2025-03-27 04:07:37 +0530

    chore: separate io and logic

--- internal/models/grobbles.go
+++ internal/models/grobbles.go
@@ -18,6 +18,69 @@ type Grobble struct {
 	SongDuration int    `json:"duration_ms"`
 }
 
+type SpotifyResponse struct {
+	Items []struct {
+		Track struct {
+			Name         string `json:"name"`
+			Artists      []struct {
+				Name string `json:"name"`
+			} `json:"artists"`
+			SongDuration int `json:"duration_ms"`
+		} `json:"track"`
+		PlayedAt string `json:"played_at"`
+	} `json:"items"`
+}
+
+func SpotifyRecentlyPlayed(username string) (*http.Response, error) {
+	AccountsMu.RLock()
+	account, exists := Accounts[username]
+	AccountsMu.RUnlock()
+	if !exists {
+		return nil, fmt.Errorf("account not found")
+	}
+
+	req, err := http.NewRequest("GET", "https://api.spotify.com/v1/me/player/recently-played", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+account.AccessToken)
+
+	client := &http.Client{}
+	return client.Do(req)
+}
+
+func ParseSpotifyResponse(body io.Reader) ([]Grobble, error) {
+	var sr SpotifyResponse
+	if err := json.NewDecoder(body).Decode(&sr); err != nil {
+		return nil, err
+	}
+
+	var grobbles []Grobble
+	for _, item := range sr.Items {
+		// Ensure that there is at least one artist.
+		if len(item.Track.Artists) == 0 {
+			continue
+		}
+		grobbles = append(grobbles, Grobble{
+			Track:        item.Track.Name,
+			Artist:       item.Track.Artists[0].Name,
+			PlayedAt:     item.PlayedAt,
+			SongDuration: item.Track.SongDuration,
+		})
+	}
+	return grobbles, nil
+}
+
+func GetUserGrobbles(username string) ([]Grobble, error) {
+	resp, err := SpotifyRecentlyPlayed(username)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ParseSpotifyResponse(resp.Body)
+}
+
 func LoadGrobbles() {
 	file, err := os.Open(GrobblesFile)
 	if err != nil {
@@ -34,7 +97,7 @@ func LoadGrobbles() {
 	}
 }
 
-func SaveGrobbles() {
+func SaveGrobbles(Grobbles map[string][]Grobble) {
 	GrobblesMu.RLock()
 	defer GrobblesMu.RUnlock()
 
@@ -77,61 +140,8 @@ func FetchGrobbles() {
 			GrobblesMu.Unlock()
 		}
 
-		SaveGrobbles()
-
+		SaveGrobbles(Grobbles)
 		log.Println("Grobbles updated!")
 		time.Sleep(5 * time.Minute)
 	}
-}
-
-func GetUserGrobbles(username string) ([]Grobble, error) {
-	AccountsMu.RLock()
-	account, exists := Accounts[username]
-	AccountsMu.RUnlock()
-	if !exists {
-		return nil, fmt.Errorf("account not found")
-	}
-
-	req, err := http.NewRequest("GET", "https://api.spotify.com/v1/me/player/recently-played", nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", "Bearer "+account.AccessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-	var response struct {
-		Items []struct {
-			Track struct {
-				Name    string `json:"name"`
-				Artists []struct {
-					Name string `json:"name"`
-				} `json:"artists"`
-				SongDuration int `json:"duration_ms"`
-			} `json:"track"`
-			PlayedAt string `json:"played_at"`
-		} `json:"items"`
-	}
-
-	if err := json.Unmarshal(body, &response); err != nil {
-		return nil, err
-	}
-
-	var Grobbles []Grobble
-	for _, item := range response.Items {
-		Grobbles = append(Grobbles, Grobble{
-			Track:        item.Track.Name,
-			Artist:       item.Track.Artists[0].Name,
-			PlayedAt:     item.PlayedAt,
-			SongDuration: item.Track.SongDuration,
-		})
-	}
-
-	return Grobbles, nil
 }
